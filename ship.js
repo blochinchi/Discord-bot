16886e240023839b5fb85312cccbb35c753b316e
blochinchi <59876671+blochinchi@users.noreply.github.com>
2020-09-01 13:56:00 +0530

    Update ship.js

--- ship.js
+++ ship.js
@@ -4,4 +4,4 @@ var yourCrushesName = prompt("whats your crushes name");
 console.log(yourCrushesName);
 var ship = Math.random();
 ship = Math.floor(ship*101);
-alert("the love score of " + yourName + " and " + yourCrushesName + " is " + ship);
+alert("the love score of " + yourName + " and " + yourCrushesName + " is " + ship + "%");
