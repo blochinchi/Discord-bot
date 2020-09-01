fecca953b0db84ccc1492ae136cbedb3c6499552
blochinchi <59876671+blochinchi@users.noreply.github.com>
2020-09-01 13:53:57 +0530

    Create ship.js

---
+++ ship.js
@@ -0,0 +1,7 @@
+var yourName = prompt("whats your name");
+console.log(yourName);
+var yourCrushesName = prompt("whats your crushes name");
+console.log(yourCrushesName);
+var ship = Math.random();
+ship = Math.floor(ship*101);
+alert("the love score of " + yourName + " and " + yourCrushesName + " is " + ship);
